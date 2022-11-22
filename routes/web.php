a87450ef0c2e4fedc9e064f858639de967d5968f
ardilalukita14 <lukitasari623@gmail.com>
2022-11-22 11:21:51 +0700

    Perbaikan routing

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,9 @@ route::get('/profil-kota-madiun',[\App\Http\Controllers\FrontendController::clas
 route::get('/sejarah-kota-madiun',[\App\Http\Controllers\FrontendController::class,'sejarah'])->name('layouts.frontend.sejarahkota');
 
 /** Slug */
-Route::get('/berita/{year}/{month}/{day}/{slug}', [App\Http\Controllers\HomeController::class, 'contents_blog'] )->name('contents_blog');
-Route::get('/kategori/{slug}', [App\Http\Controllers\HomeController::class, 'contents_kategori'] )->name('contents_kategori');
-Route::get('/{slug}', [App\Http\Controllers\HomeController::class, 'content_kategori'] )->name('content_kategori');
+// Route::get('/berita/{year}/{month}/{day}/{slug}', [App\Http\Controllers\HomeController::class, 'contents_blog'] )->name('contents_blog');
+// Route::get('/kategori/{slug}', [App\Http\Controllers\HomeController::class, 'contents_kategori'] )->name('contents_kategori');
+// Route::get('/{slug}', [App\Http\Controllers\HomeController::class, 'content_kategori'] )->name('content_kategori');
 
 /** Dashboard Admin */
 Route::group(['middleware'=>['admin','auth','PreventBackHistory']], function(){
