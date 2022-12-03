f79e94553e2c67d17d969eb2f616cf1a9280eb4d
eksalailia <eksalailia@gmail.com>
2022-12-03 17:36:11 +0700

    halaman berita

--- resources/views/user/kategori/base.blade.php
+++ resources/views/user/kategori/base.blade.php
@@ -36,24 +36,6 @@
   <div class="container">
     <div class="row">
 
-      <div class="col-md-6 wow slideInUp" data-wow-delay="0.1s">
-        <div class="blog-item bg-light rounded overflow-hidden">
-            <div class="blog-img position-relative overflow-hidden">
-                <img class="img-fluid" src="img/blog-1.jpg" alt="">
-                <a class="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4" href="">Web Design</a>
-            </div>
-            <div class="p-4">
-                <div class="d-flex mb-3">
-                    <small class="me-3"><i class="far fa-user text-primary me-2"></i>John Doe</small>
-                    <small><i class="far fa-calendar-alt text-primary me-2"></i>01 Jan, 2045</small>
-                </div>
-                <h4 class="mb-3">How to build a website</h4>
-                <p>Dolor et eos labore stet justo sed est sed sed sed dolor stet amet</p>
-                <a class="text-uppercase" href="">Read More <i class="bi bi-arrow-right"></i></a>
-            </div>
-        </div>
-    </div>
-
       @if ($posts != null)
       <div class="col-lg-8 row g-5">
         @foreach ($posts as $beritaItem)
