a0bdb358bfed849e3542ded344c48a43cc794f4c
ardilalukita14 <lukitasari623@gmail.com>
2022-12-04 12:19:56 +0700

    Perbaikan halaman utama (layout)

--- resources/views/user/berita/index.blade.php
+++ resources/views/user/berita/index.blade.php
@@ -70,11 +70,11 @@
     <div class="row">
         <div class="col-lg-6">
           <div class="ts-intro">
-              <h2 class="into-title">About Us</h2>
-              <h8 class="into-sub-title">We deliver landmark projects</h8>
-              <p>We are rethoric question ran over her cheek When she reached the first hills of the Italic Mountains,
-                she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village
-                and the subline of her own road, the Line Lane.</p>
+              <p class="into-title">About Us</p>
+              <h8 class="into-sub-title" style="font-size: 36px; margin-bottom: 20px; text-transform: uppercase; letter-spacing: -0.5px; color: #212121; font-weight: 700; font-family: Montserrat, sans-serif;
+                text-rendering: optimizeLegibility;">PPID PELAKSANA KOTA MADIUN</h8>
+              <p style="text-align:justify;">Pejabat Pengelola Informasi dan Dokumentasi (PPID) adalah pejabat yang bertanggung jawab di bidang penyimpanan, 
+                  pendokumentasian, penyediaan dan/ atau pelayanan informasi di badan publik.</p>
           </div><!-- Intro box end -->
 
           <div class="gap-20"></div>
@@ -86,7 +86,7 @@
                       <i class="fas fa-trophy"></i>
                     </span>
                     <div class="ts-service-box-content">
-                      <h3 class="service-box-title">We've Repution for Excellence</h3>
+                      <h3 class="service-box-title" style="color: #555; text-align:justify;">Menyediakan informasi yang dibutuhkan publik</h3>
                     </div>
                 </div><!-- Service 1 end -->
               </div><!-- col end -->
@@ -97,7 +97,7 @@
                       <i class="fas fa-sliders-h"></i>
                     </span>
                     <div class="ts-service-box-content">
-                      <h3 class="service-box-title">We Build Partnerships</h3>
+                      <h3 class="service-box-title" style="color: #555; text-align:justify;">Meningkatkan pengelolaan dan pelayanan informasi</h3>
                     </div>
                 </div><!-- Service 2 end -->
               </div><!-- col end -->
@@ -110,7 +110,7 @@
                       <i class="fas fa-thumbs-up"></i>
                     </span>
                     <div class="ts-service-box-content">
-                      <h3 class="service-box-title">Guided by Commitment</h3>
+                      <h3 class="service-box-title" style="color: #555; text-align:justify;">Membangun dan mengembangkan sistem informasi penyediaan dan layanan informasi</h3>
                     </div>
                 </div><!-- Service 1 end -->
               </div><!-- col end -->
@@ -121,7 +121,7 @@
                       <i class="fas fa-users"></i>
                     </span>
                     <div class="ts-service-box-content">
-                      <h3 class="service-box-title">A Team of Professionals</h3>
+                      <h3 class="service-box-title" style="color: #555; text-align:justify;">Meningkatkan kompetensi sumberdaya manusia</h3>
                     </div>
                 </div><!-- Service 2 end -->
               </div><!-- col end -->
@@ -129,56 +129,13 @@
         </div><!-- Col end -->
 
         <div class="col-lg-6 mt-4 mt-lg-0">
-          <h8 class="into-sub-title">Our Values</h8>
-          <p>Minim Austin 3 wolf moon scenester aesthetic, umami odio pariatur bitters. Pop-up occaecat taxidermy street art, tattooed beard literally.</p>
+                <div id="gpr-kominfo-widget-container">
+                    <div class="card-body">
+                        <script type="text/javascript" src="https://widget.kominfo.go.id/gpr-widget-kominfo.min.js"></script>
+                    </div>
+                </div>
 
-          <div class="accordion accordion-group" id="our-values-accordion">
-              <div class="card">
-                <div class="card-header p-0 bg-transparent" id="headingOne">
-                    <h2 class="mb-0">
-                      <button class="btn btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                          Safety
-                      </button>
-                    </h2>
-                </div>
-              
-                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#our-values-accordion">
-                    <div class="card-body">
-                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidata
-                    </div>
-                </div>
-              </div>
-              <div class="card">
-                <div class="card-header p-0 bg-transparent" id="headingTwo">
-                    <h2 class="mb-0">
-                      <button class="btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                          Customer Service
-                      </button>
-                    </h2>
-                </div>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#our-values-accordion">
-                    <div class="card-body">
-                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidata
-                    </div>
-                </div>
-              </div>
-              <div class="card">
-                <div class="card-header p-0 bg-transparent" id="headingThree">
-                    <h2 class="mb-0">
-                      <button class="btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                          Integrity
-                      </button>
-                    </h2>
-                </div>
-                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#our-values-accordion">
-                    <div class="card-body">
-                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidata
-                    </div>
-                </div>
-              </div>
-          </div>
-          <!--/ Accordion end -->
-
+    </div>
         </div><!-- Col end -->
     </div><!-- Row end -->
   </div><!-- Container end -->
