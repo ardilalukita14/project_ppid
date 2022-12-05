9d3df28025bdf9b40a24fb5403aa6724e4748ef2
eksalailia <eksalailia@gmail.com>
2022-12-05 14:10:31 +0700

    modifikasi chart

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -125,18 +125,18 @@
         </div>
       </div>
     </div>
-    <br> 
+    <br>
     <div class="row">
       <div class="col-xl-6">
-          <div class="card mb-4" style="background-color: #aed6f1; margin-left:-12px;">
+          <div class="card mb-4" style="background-color: #d4c7b4; margin-left:-12px;">
               <div class="card-header" >
                   <i class="fas fa-chart-area me-1"></i><b style="margin-left:5px;"> Jumlah Berita Ter-Publish Per Bulan</b>
               </div>
-          <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
+          <div class="card-body" ><canvas id="myBarChart" width="100%" height="40"></canvas></div>
           </div>
       </div>
       <div class="col-xl-6">
-        <div class="card mb-4" style="background-color: #aed6f1; margin-left:-12px;">
+        <div class="card mb-4" style="background-color: #d4c7b4; margin-left:-12px;">
             <div class="card-header" >
                 <i class="fas fa-chart-area me-1"></i><b style="margin-left:5px;"> Jumlah Berita Ter-Publish Per Tahun</b>
                     </div>
@@ -183,7 +183,7 @@
     </div>
   </div>
 
-  
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
 <script type="text/javascript">
 	var _ydata=JSON.parse('{!! json_encode($months) !!}');
