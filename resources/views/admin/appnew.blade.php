3f7f37d7ae2bf764a94e57fcee6a2d6704ec5a7d
ardilalukita14 <lukitasari623@gmail.com>
2023-01-04 12:38:47 +0700

    pop up image

--- resources/views/admin/appnew.blade.php
+++ resources/views/admin/appnew.blade.php
@@ -74,6 +74,14 @@
     });
   </script>
 
+<script>
+   $(function() {
+     $('.pop').on('click', function() {
+       $('.imagepreview').attr('src',$(this).find('img').attr('src'));
+       $('#imagemodal').modal('show');   
+       });		
+   });
+</script>
 @yield('content')
 
 
